54168d22ca3bfb785252f27b8fdefa6c245d0149
TonyChouteau <tony.chouteau@outlook.fr>
2020-02-12 22:08:02 +0100

    added comments

--- sequence1.py
+++ sequence1.py
@@ -17,11 +17,15 @@ import random as r
 #====================================
 # Définition des variables
 
+#Taille de matrice
 N1, N2 = (200,100)
+#Taux de remplissage
 rate = 1/3
 
+#Création de la matrice via numpy (matrice pleine de 0)
 surface = np.zeros([N2,N1], dtype=bool)
 
+#Création d'une liste contenant les positions des cases vides (toutes au début)  
 emptyCells = []
 for i in range(N2):
     for j in range(N1):
@@ -30,9 +34,9 @@ for i in range(N2):
 #====================================
 # Functions
 
+### Fill
 
-# Fill
-
+#Fonction de reset de la matrice et de la liste des cases vides
 def reset(surface, emptyCells):
 
     for i in range(N2):
@@ -44,12 +48,10 @@ def reset(surface, emptyCells):
         for j in range(N1):
             emptyCells.append((i,j))
 
-#
-
+#Fonction pour remplir la matrice avec {rate}% de cases
 def fillWithRate(surface, emptyCells, N1, N2, rate):
     
     tN = int(N1*N2*rate)
-    print(tN)
     #newEmptyCells = list(emptyCells)
 
     for _ in range(tN):
@@ -57,19 +59,16 @@ def fillWithRate(surface, emptyCells, N1, N2, rate):
         (y,x) = emptyCells[randomIndex]
         emptyCells.pop(randomIndex)
         surface[y][x] = True
-        #pltShow(surface)
-        #print(surface)
-        #print(emptyCells)
 
-# Show
+### Show
 
+#Afficher la matrice dans une fenêtre
 def pltShow(surface):
     plt.figure(figsize=(15,7))
     plt.matshow(surface, fignum=1, vmin=0, vmax=1)
     plt.show()
 
-#
-
+#Affiche la matrice dans la console
 def npShow(surface):
     print(surface)
     print()
@@ -78,6 +77,7 @@ def npShow(surface):
 #====================================
 # Entry Point
 
+#Foncion principale
 def main():
     print("Lancement")
     print("Affichage matrice vide")
@@ -88,7 +88,6 @@ def main():
     print("Affichage matrice pleine à "+str(rate*100)+"%")
     pltShow(surface)
 
-#
-
+#Point d'entrée du programme, il lance la fonction "main"
 if __name__ == "__main__":
     main()
